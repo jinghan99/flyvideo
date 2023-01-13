afa664913f1c14075e32afcfd283c7d7f041ce54
jinghan <jinghan94>
2023-01-13 23:10:35 +0800

    first commit

---
+++ app/src/main/java/top/jinghan94/flyvideo/logic/network/ServiceCreator.kt
@@ -0,0 +1,19 @@
+package top.jinghan94.flyvideo.logic.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ServiceCreator {
+
+    private const val BASE_URL = "https://cj.lziapi.com/"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun <T> create(serviceClass: Class<T>): T = retrofit.create(serviceClass)
+
+    inline fun <reified T> create(): T = create(T::class.java)
+
+}
